417724cfd49cce4a20274423429b9e16017cf5f4
uhuycihuy <ridhobujank69@gmail.com>
2025-03-23 12:25:32 +0700

    perubahan main-konfigurasi theme
    iki tugase bapak hendro

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'home_page.dart';
 import 'detail_page.dart';
 
@@ -11,20 +12,164 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // Tema Light
+    // Konfigurasi tema light
+    ThemeData myLightTheme = ThemeData(
+      useMaterial3: true,
+      primaryColor: Colors.blue,
+      hintColor: Colors.orange,
+      fontFamily: 'Roboto',
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.blue,
         brightness: Brightness.light,
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      darkTheme: ThemeData(
-        // Tema Dark
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.blue,
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ),
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      scaffoldBackgroundColor: Colors.white,
+      textTheme: const TextTheme(
+        titleLarge: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        titleMedium: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        titleSmall: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        bodyLarge: TextStyle(fontSize: 20),
+        bodyMedium: TextStyle(fontSize: 14),
+        bodySmall: TextStyle(fontSize: 14),
+      ),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        shape: CircleBorder(),
+        backgroundColor: Colors.orange,
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(foregroundColor: Colors.blue),
+      ),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: OutlinedButton.styleFrom(
+          side: const BorderSide(color: Colors.blue),
+          foregroundColor: Colors.blue,
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: WidgetStateProperty.all<Color>(Colors.blue),
+          foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        filled: true,
+        fillColor: Colors.white,
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+        labelStyle: const TextStyle(fontSize: 15),
+        hintStyle: const TextStyle(fontSize: 10),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide.none,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: const BorderSide(color: Colors.grey),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: const BorderSide(color: Colors.blue),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: const BorderSide(color: Colors.red),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: const BorderSide(color: Colors.red),
+        ),
+      ),
+    );
+
+    // Konfigurasi tema dark
+    ThemeData myDarkTheme = ThemeData(
+      useMaterial3: true,
+      primaryColor: const Color(0xff283449),
+      hintColor: const Color(0xffffc054),
+      fontFamily: 'Roboto',
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.indigo,
         brightness: Brightness.dark,
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Color(0xff283449),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ),
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      scaffoldBackgroundColor: const Color(0xff3d485b),
+      textTheme: const TextTheme(
+        titleLarge: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white70),
+        titleMedium: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white70),
+        titleSmall: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white70),
+        bodyLarge: TextStyle(fontSize: 20, color: Colors.white70),
+        bodyMedium: TextStyle(fontSize: 14, color: Colors.white70),
+        bodySmall: TextStyle(fontSize: 14, color: Colors.white70),
+      ),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        shape: CircleBorder(),
+        backgroundColor: Color(0xff633e73),
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(foregroundColor: Colors.white70),
+      ),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: OutlinedButton.styleFrom(
+          side: const BorderSide(color: Colors.white70),
+          foregroundColor: Colors.white70,
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: WidgetStateProperty.all<Color>(const Color(0xff283449)),
+          foregroundColor: WidgetStateProperty.all<Color>(Colors.white70),
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        filled: true,
+        fillColor: const Color(0xff283449),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+        labelStyle: const TextStyle(fontSize: 15, color: Colors.white54),
+        hintStyle: const TextStyle(fontSize: 15, color: Color(0xff633e73)),
+        floatingLabelStyle: const TextStyle(fontSize: 20, color: Color(0xff633e73)),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide.none,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: const BorderSide(color: Color(0xff633e73)),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: const BorderSide(color: Color(0xff633e73)),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: const BorderSide(color: Colors.orangeAccent),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: const BorderSide(color: Colors.orangeAccent),
+        ),
+      ),
+    );
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tugas Mobile Lanjut P7',
+      theme: myLightTheme,
+      darkTheme: myDarkTheme,
       themeMode: ThemeMode.system,
       home: const HomePage(),
       routes: {
